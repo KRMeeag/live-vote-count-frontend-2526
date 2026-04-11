2b90ead598e9fbd63d43c0a146232dd63e3c8a18
KRMeeag <kenthrazenmagbanua@gmail.com>
2026-04-12 05:49:13 +0800

    chore: fix imports hindering deployment

---
+++ src/features/tracker/components/index.ts
@@ -0,0 +1,7 @@
+export {default as CollegeCard} from "./CollegeCard";
+export {default as DidYouKnowWidget} from "./DidYouKnowWidget";
+export {default as FlickeringGrid} from "./FlickeringGrid";
+export {default as LeaderboardList} from "./LeaderboardList";
+export {default as OverallTurnoutCard} from "./OverallTurnoutCard";
+export {default as QrWidget} from "./QrWidget";
+
